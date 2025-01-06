18bf7de714ec8362a2cfabaa51ba205870a8486a
Even <even.berhane2022@gmail.com>
2025-01-06 17:57:58 +0300

    motion in view

--- src/app/components/ui/Lamp.tsx
+++ src/app/components/ui/Lamp.tsx
@@ -7,7 +7,12 @@ export function LampDemo() {
   return (
     <LampContainer>
       <motion.h1
-        
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
         }}
         className="mt-10 bg-gradient-to-br from-slate-300 to-slate-500 py-5 bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-5xl"
       >
