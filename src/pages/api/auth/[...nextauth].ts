fd670ce769b4f24973ab161bb26b27c230a7d48a
Even <even.berhane2022@gmail.com>
2025-01-07 15:23:23 +0300

    auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,11 @@
-import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+
+const scopes = [
+  "user-read-email",
+  "user-top-read",
+  "user-read-recently-played",
+].join(",")
 
 export default NextAuth({
   providers: [
@@ -7,11 +13,23 @@ export default NextAuth({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
       authorization: {
-        params: {
-          scope: "user-read-email user-top-read user-read-recently-played",
-        },
+        params: { scope: scopes },
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+        token.expiresAt = account.expires_at
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
+})
+
