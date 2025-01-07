76631818383bf1aa0fc532a587f205055ff8d432
Even <even.berhane2022@gmail.com>
2025-01-07 15:30:30 +0300

    expicitly cast string

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
 const scopes = [
@@ -7,7 +7,7 @@ const scopes = [
   "user-read-recently-played",
 ].join(",");
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
@@ -20,15 +20,18 @@ export default NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.expiresAt = account.expires_at;
+        token.accessToken = account.access_token as string; // Explicitly cast to string
+        token.refreshToken = account.refresh_token as string; // Explicitly cast to string
+        token.expiresAt = account.expires_at as number; // Explicitly cast to number
       }
       return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken;
+      session.accessToken = token.accessToken as string; // Explicitly cast to string
       return session;
     },
   },
-});
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
